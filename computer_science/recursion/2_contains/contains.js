66f394e29ecc2305d50b880753a557537f7a5927
Ali-Al-Khuwaja <132617733+Ali-Al-Khuwaja@users.noreply.github.com>
2026-06-23 22:38:40 +0300

    solved

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,17 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+  for (let key in object) {
+    if (Object.is(object[key], target)) {
+      return true;
+    }
+
+    if (object[key] instanceof Object) {
+      if (contains(object[key], target)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
-  
 // Do not edit below this line
 module.exports = contains;

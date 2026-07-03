f14887b62f044a96537bae4b31410cb5fcdb0cb3
Ali-Al-Khuwaja <132617733+Ali-Al-Khuwaja@users.noreply.github.com>
2026-07-03 23:59:56 +0300

    solved

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,16 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length === 0) return [[]];
+
+  let result = [];
+  for (let element of arr) {
+    let cut = arr.filter((item) => item !== element); // create an array without element
+    let combos = permutations(cut);
+    for (let combo of combos) {
+      result.push([element, ...combo]);
+    }
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
